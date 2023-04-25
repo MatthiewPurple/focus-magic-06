b5dda972510b38fdceedd7e8168d7564aff859d6
Matthiew Purple <matthiewpurple@hotmail.com>
2023-04-25 15:59:32 +0200

    removed focused healing

--- FocusMagic06.cs
+++ FocusMagic06.cs
@@ -9,14 +9,26 @@ using focus_magic_06;
 namespace focus_magic_06;
 public class FocusMagic06 : MelonMod
 {
+    static public bool isHealing = false;
+
+    // After checking if a certain buff/debuff is applied
+    [HarmonyPatch(typeof(nbCalc), nameof(nbCalc.nbGetMagicAttack))]
+    private class Patch
+    {
+        public static void Postfix(ref int nskill)
+        {
+            isHealing = datSkill.tbl[nskill].skillattr == 13;
+        }
+    }
+
     // After checking if a certain buff/debuff is applied
     [HarmonyPatch(typeof(nbCalc), nameof(nbCalc.nbGetHojoRitu))]
-    private class Patch
+    private class Patch2
     {
         public static void Postfix(ref int formindex, ref int type, ref float __result)
         {
             // If the game is checking for the presence of magic buff (5) and the demon has focused (15)
-            if (type == 5 && nbMainProcess.nbGetPartyFromFormindex(formindex).count[15] == 1)
+            if (type == 5 && nbMainProcess.nbGetPartyFromFormindex(formindex).count[15] == 1 && !isHealing)
             {
                 nbMainProcess.nbGetPartyFromFormindex(formindex).count[15] = 0; // Remove Focus
                 __result *= 2.5f; // Multiply damage by 2.5
@@ -26,7 +38,7 @@ public class FocusMagic06 : MelonMod
 
     // After getting the description of a skill
     [HarmonyPatch(typeof(datSkillHelp_msg), nameof(datSkillHelp_msg.Get))]
-    private class Patch2
+    private class Patch3
     {
         public static void Postfix(ref int id, ref string __result)
         {
